b58aa8688702c2d57613b43ce3b4b0812214eade
MrShameer <67554400+MrShameer@users.noreply.github.com>
2021-01-28 18:07:54 +0800

    Update meme.py

--- meme.py
+++ meme.py
@@ -5,7 +5,7 @@ from Database import *
 from twilio.rest import Client
 
 from quoters import Quote #tak bole search tapi anime
-from quote.quote import quote as qu #bole search
+from quote import quote as qu #bole search
 #import quote as qu
 
 account_sid = os.getenv('SID')
